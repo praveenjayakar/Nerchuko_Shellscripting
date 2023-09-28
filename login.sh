590f7f846358c253e0e6b1446e721a3a72f74207
Pj <Praveenjayakar7@gmail.com>
2023-09-28 21:16:15 +0530

    starting adding shell script topics

---
+++ login.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+echo "Hello user Enter your email: "
+read email
+
+echo "Enter your password: "
+read -s password
+
+echo "Login successfull"
---
+++ takeinput.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Enter your name: "
+read name
+
+echo "Thanks for using the script $name "
